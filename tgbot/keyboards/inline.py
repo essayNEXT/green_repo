abf3d4a82bce829105c4d05331d166e052fd8693
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-02-22 10:08:40 +0100

    deleted: all materials from branch GGG

--- tgbot/keyboards/inline.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-present_button = InlineKeyboardButton(text="ПРИСУТНІЙ", callback_data="present")
-absent_button = InlineKeyboardButton(text="ВІДСУТНІЙ", callback_data="absent")
-
-main_keyboard = InlineKeyboardMarkup(row_width=1).add(present_button, absent_button)
-
-
